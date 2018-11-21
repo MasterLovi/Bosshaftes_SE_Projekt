eb781f57b7592d0957833961b03e5af8121dead2
Leon Klimpel <l.klimpel@sap.com>
2018-11-21 16:41:00 +0100

    route test data

--- src/listener/ContextListener.java
+++ src/listener/ContextListener.java
@@ -2,6 +2,9 @@ package listener;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
 import util.Time;
 
 import javax.persistence.EntityManager;
@@ -12,6 +15,7 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import model.Location;
+import model.Route;
 import model.Users;
 
 /**
@@ -84,6 +88,7 @@ public class ContextListener implements ServletContextListener {
 			location.setTime(time);
 			location.setLatitude(49.4775206);
 			location.setLongitude(8.4219807);
+			location.setPictures((List<byte[]>) new ArrayList<byte[]>());
 			em.persist(location);
 			Location location2 = new Location();
 			location2.setName("Hochschule Ludwigshafen 2");
@@ -92,6 +97,7 @@ public class ContextListener implements ServletContextListener {
 			location2.setTime(time);
 			location2.setLatitude(49.47303236240146);
 			location2.setLongitude(8.394641872728245);
+			location.setPictures((List<byte[]>) new ArrayList<byte[]>());
 			em.persist(location2);
 			Location location3 = new Location();
 			location3.setName("Hochschule Ludwigshafen 3");
@@ -100,6 +106,7 @@ public class ContextListener implements ServletContextListener {
 			location3.setTime(time);
 			location3.setLatitude(49.45674385539652);
 			location3.setLongitude(8.41655731201172);
+			location.setPictures((List<byte[]>) new ArrayList<byte[]>());
 			em.persist(location3);
 			Location location4 = new Location();
 			location4.setName("Hochschule Ludwigshafen 4");
@@ -108,7 +115,30 @@ public class ContextListener implements ServletContextListener {
 			location4.setTime(time);
 			location4.setLatitude(49.47035517151213);
 			location4.setLongitude(8.44139098422602);
+			location.setPictures((List<byte[]>) new ArrayList<byte[]>());
 			em.persist(location4);
+			
+			Route route = new Route();
+			route.setName("Tims Duftpfad");
+			route.setDescription("Route über alle Punkte an denen Tim stinkt/gestunken hat/stinken wird.");
+			route.setAvgRating(0);
+			route.setFirstLat(location.getLatitude());
+			route.setFirstLong(location.getLongitude());
+			route.setNumberOfStops(4);
+			route.setOwner(user);
+			route.setTimeString("03:30:00");
+			route.setTime(time);
+			route.setPictures((List<byte[]>) new ArrayList<byte[]>());
+			route.setType("Party");
+			
+			List<Location> l = new ArrayList<Location>();
+			l.add(location);
+			l.add(location2);
+			l.add(location3);
+			l.add(location4);
+			route.setStops(l);
+			
+			em.persist(route);
 			em.getTransaction().commit();
 
 		} catch (Exception e) {
